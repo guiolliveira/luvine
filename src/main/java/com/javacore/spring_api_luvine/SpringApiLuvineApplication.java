9613cd4a77b9e851b6dd63590fa4f0adc4322ef3
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-08 17:04:41 -0300

    first commit

---
+++ src/main/java/com/javacore/spring_api_luvine/SpringApiLuvineApplication.java
@@ -0,0 +1,13 @@
+package com.javacore.spring_api_luvine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiLuvineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiLuvineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javacore/spring_api_luvine/SpringApiLuvineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacore.spring_api_luvine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiLuvineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
