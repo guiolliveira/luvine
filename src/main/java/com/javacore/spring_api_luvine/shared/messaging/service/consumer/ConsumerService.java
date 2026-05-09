43ebea008210a589b44718298eb03418db258b33
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-09 18:04:38 -0300

    Add loggin to ConsumerService.java

--- src/main/java/com/javacore/spring_api_luvine/shared/messaging/service/consumer/ConsumerService.java
+++ src/main/java/com/javacore/spring_api_luvine/shared/messaging/service/consumer/ConsumerService.java
@@ -4,10 +4,13 @@ import com.javacore.spring_api_luvine.notification.dto.MailSenderRequest;
 import com.javacore.spring_api_luvine.notification.service.MailSenderService;
 import com.javacore.spring_api_luvine.shared.messaging.config.RabbitMQDLQConfig;
 import com.javacore.spring_api_luvine.shared.messaging.dto.EmailMessageRequest;
+import com.javacore.spring_api_luvine.shared.util.EmailMask;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ConsumerService {
@@ -16,10 +19,19 @@ public class ConsumerService {
 
     @RabbitListener(queues = RabbitMQDLQConfig.EMAIL_QUEUE)
     public void consumer(EmailMessageRequest request) {
-        mailSenderService.sendEmail(new MailSenderRequest(
-                request.to(),
-                request.subject(),
-                request.body()
-        ));
+        String maskedEmail = EmailMask.mask(request.to());
+        log.info("event=message_received queue={} to={}", RabbitMQDLQConfig.EMAIL_QUEUE, maskedEmail);
+
+        try {
+            mailSenderService.sendEmail(new MailSenderRequest(
+                    request.to(),
+                    request.subject(),
+                    request.body()
+            ));
+            log.info("event=message_processed queue={} to={}", RabbitMQDLQConfig.EMAIL_QUEUE, maskedEmail);
+        } catch (Exception ex) {
+            log.error("event=message_processing_failed queue={} to={}", RabbitMQDLQConfig.EMAIL_QUEUE, maskedEmail, ex);
+            throw ex;
+        }
     }
 }
