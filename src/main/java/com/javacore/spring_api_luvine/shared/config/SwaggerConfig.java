8ca8c8f7db2e7faca49ea6f7d66c0d388053ec88
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-09 18:29:56 -0300

    Add SwaggerConfig.java

---
+++ src/main/java/com/javacore/spring_api_luvine/shared/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.javacore.spring_api_luvine.shared.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String schemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Luvine API")
+                        .description("Documentação da API do ecossistema de e-commerce Luvine")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
