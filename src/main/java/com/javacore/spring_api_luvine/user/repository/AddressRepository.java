e1b27135be99841db161d2e8493c56966f4e727d
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-12 18:43:29 -0300

    Add AddressRepository.java

---
+++ src/main/java/com/javacore/spring_api_luvine/user/repository/AddressRepository.java
@@ -0,0 +1,26 @@
+package com.javacore.spring_api_luvine.user.repository;
+
+import com.javacore.spring_api_luvine.user.domain.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    Optional<Address> findFirstByUserPublicIdAndActiveTrueOrderByCreatedAtDesc(UUID userPublicId);
+    Optional<Address> findByPublicIdAndUserPublicId(UUID addressPublicId, UUID userPublicId);
+
+    boolean existsByUserPublicIdAndActiveTrue(UUID userPublicId);
+    List<Address> findAllByUserPublicIdAndActiveTrue(UUID userPublicId);
+
+    @Modifying
+    @Query("UPDATE Address a SET a.defaultAddress = FALSE WHERE" +
+            " a.user.publicId = :userPublicId AND a.defaultAddress = TRUE")
+    void resetDefaultAddressForUser(@Param("userPublicId") UUID userPublicId);
+}
