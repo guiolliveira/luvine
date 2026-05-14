5dcd27d7c551bec7a786daa2c635b9c0bd329cfc
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-14 17:31:06 -0300

    Update EmailMessageRequest.java

--- src/main/java/com/javacore/spring_api_luvine/shared/messaging/dto/EmailMessageRequest.java
+++ src/main/java/com/javacore/spring_api_luvine/shared/messaging/dto/EmailMessageRequest.java
@@ -1,8 +1,12 @@
 package com.javacore.spring_api_luvine.shared.messaging.dto;
 
+import java.util.Map;
+
 public record EmailMessageRequest(
         String to,
+        String name,
         String subject,
-        String body
+        String templateName,
+        Map<String, Object> variables
 ) {
 }
