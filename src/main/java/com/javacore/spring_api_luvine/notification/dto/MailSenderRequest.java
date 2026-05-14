108a36586cbc8f973091936263247ff824aba4c3
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-14 17:29:01 -0300

    Update MailSenderRequest.java

--- src/main/java/com/javacore/spring_api_luvine/notification/dto/MailSenderRequest.java
+++ src/main/java/com/javacore/spring_api_luvine/notification/dto/MailSenderRequest.java
@@ -1,8 +1,12 @@
 package com.javacore.spring_api_luvine.notification.dto;
 
+import java.util.Map;
+
 public record MailSenderRequest(
         String to,
         String name,
-        String code
+        String subject,
+        String templateName,
+        Map<String, Object> variables
 ) {
 }
