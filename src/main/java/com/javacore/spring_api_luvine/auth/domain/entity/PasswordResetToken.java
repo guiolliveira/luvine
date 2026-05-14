1c1be743704c5ad10db6aa3144f0819b4317a5ac
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-14 17:06:19 -0300

    Add PasswordResetToken entity

---
+++ src/main/java/com/javacore/spring_api_luvine/auth/domain/entity/PasswordResetToken.java
@@ -0,0 +1,62 @@
+package com.javacore.spring_api_luvine.auth.domain.entity;
+
+import com.javacore.spring_api_luvine.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+@Entity
+@Table(name = "password_reset_tokens")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String token;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false, updatable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private boolean used;
+
+    @Column(nullable = false)
+    private String deviceInfo;
+
+    @Column(nullable = false)
+    private String ipAddress;
+
+    private PasswordResetToken(User user, String token, String deviceInfo, String ipAddress) {
+        this.user = user;
+        this.token = token;
+        this.createdAt = Instant.now();
+        this.expiresAt = this.createdAt.plus(15, ChronoUnit.MINUTES);
+        this.used = false;
+        this.deviceInfo = deviceInfo;
+        this.ipAddress = ipAddress;
+    }
+
+    public static PasswordResetToken create(User user, String token, String deviceInfo, String ipAddress) {
+        return new PasswordResetToken(user, token, deviceInfo, ipAddress);
+    }
+
+    public void markAsUsed() {
+        this.used = true;
+    }
+}
