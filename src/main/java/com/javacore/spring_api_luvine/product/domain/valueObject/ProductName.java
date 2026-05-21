14786150d85d85ca517882f3a428959115694d02
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-21 20:31:16 -0300

    Add ProductName.java

---
+++ src/main/java/com/javacore/spring_api_luvine/product/domain/valueObject/ProductName.java
@@ -0,0 +1,32 @@
+package com.javacore.spring_api_luvine.product.domain.valueObject;
+
+import com.javacore.spring_api_luvine.product.domain.exception.InvalidProductNameException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ProductName(@Column String value) {
+    public ProductName(String value) {
+        if (value == null || value.isBlank()) {
+            throw new InvalidProductNameException();
+        }
+
+        String normalized = normalize(value);
+
+        if (!isValid(normalized)) {
+            throw new InvalidProductNameException();
+        }
+
+        this.value = normalized;
+    }
+
+    private static String normalize(String productName) {
+        return productName.trim().replaceAll("\\s+", " ");
+    }
+
+    private static boolean isValid(String productName) {
+        return !productName.isBlank()
+                && productName.length() >= 3
+                && productName.matches("^[\\p{L}\\s\\-.,()]+$");
+    }
+}
