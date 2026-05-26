65c613db6415d98bbe0617c2f26345a17ce5b329
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-26 19:38:23 -0300

    Add CategoryRepository.java

---
+++ src/main/java/com/javacore/spring_api_luvine/product/infrastructure/repository/CategoryRepository.java
@@ -0,0 +1,23 @@
+package com.javacore.spring_api_luvine.product.infrastructure.repository;
+
+import com.javacore.spring_api_luvine.product.domain.entity.Category;
+import com.javacore.spring_api_luvine.product.domain.valueObject.CategoryName;
+import com.javacore.spring_api_luvine.product.domain.valueObject.Slug;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+
+    Optional<Category> findByPublicId(UUID publicId);
+
+    Optional<Category> findBySlug(Slug slug);
+
+    boolean existsBySlug(Slug slug);
+
+    boolean existsByCategoryName(CategoryName categoryName);
+}
