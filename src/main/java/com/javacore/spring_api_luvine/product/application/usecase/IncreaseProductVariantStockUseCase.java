4faf30625c5b8b582c7b9195527f390a712d3240
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-27 19:00:01 -0300

    Add IncreaseProductVariantStockUseCase.java

---
+++ src/main/java/com/javacore/spring_api_luvine/product/application/usecase/IncreaseProductVariantStockUseCase.java
@@ -0,0 +1,36 @@
+package com.javacore.spring_api_luvine.product.application.usecase;
+
+import com.javacore.spring_api_luvine.common.config.UseCase;
+import com.javacore.spring_api_luvine.product.application.dto.ProductVariantResponse;
+import com.javacore.spring_api_luvine.product.application.dto.UpdateVariantStockRequest;
+import com.javacore.spring_api_luvine.product.application.mapper.ProductMapper;
+import com.javacore.spring_api_luvine.product.domain.entity.Product;
+import com.javacore.spring_api_luvine.product.domain.entity.ProductVariant;
+import com.javacore.spring_api_luvine.product.domain.exception.ProductNotFoundException;
+import com.javacore.spring_api_luvine.product.domain.valueObject.StockQuantity;
+import com.javacore.spring_api_luvine.product.infrastructure.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@UseCase
+@RequiredArgsConstructor
+public class IncreaseProductVariantStockUseCase {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Transactional
+    public ProductVariantResponse execute(
+            UUID productPublicId, UUID variantPublicId, UpdateVariantStockRequest request) {
+        Product product = productRepository.findByPublicId(productPublicId)
+                .orElseThrow(ProductNotFoundException::new);
+
+        ProductVariant variant = product.findVariantByPublicId(variantPublicId);
+
+        variant.increaseStock(new StockQuantity(request.stockQuantity()));
+
+        return productMapper.toProductVariantResponse(variant);
+    }
+}
