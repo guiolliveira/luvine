22adbc806b3e3c7975bc4aa28c303bd25e5edd98
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-05-28 20:53:50 -0300

    Add RemoveProductVariantUseCase.java

---
+++ src/main/java/com/javacore/spring_api_luvine/product/application/usecase/RemoveProductVariantUseCase.java
@@ -0,0 +1,33 @@
+package com.javacore.spring_api_luvine.product.application.usecase;
+
+import com.javacore.spring_api_luvine.common.config.UseCase;
+import com.javacore.spring_api_luvine.product.application.dto.ProductVariantResponse;
+import com.javacore.spring_api_luvine.product.application.mapper.ProductMapper;
+import com.javacore.spring_api_luvine.product.domain.entity.Product;
+import com.javacore.spring_api_luvine.product.domain.entity.ProductVariant;
+import com.javacore.spring_api_luvine.product.domain.exception.ProductNotFoundException;
+import com.javacore.spring_api_luvine.product.infrastructure.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@UseCase
+@RequiredArgsConstructor
+public class RemoveProductVariantUseCase {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Transactional
+    public ProductVariantResponse execute(UUID productPublicId, UUID variantPublicId) {
+        Product product = productRepository.findByPublicId(productPublicId)
+                .orElseThrow(ProductNotFoundException::new);
+
+        ProductVariant variant = product.findVariantByPublicId(variantPublicId);
+
+        product.removeVariant(variant);
+
+        return productMapper.toProductVariantResponse(variant);
+    }
+}
